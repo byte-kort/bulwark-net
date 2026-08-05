9911d090b81791f9c6deb13d457a887e08e76412
ramik12345 <rramikpv@gmail.com>
2026-08-05 22:39:04 +0300

    msg

--- server/socket.go
+++ server/socket.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"net"
-	"os"
 )
 
 type jsonData struct {
@@ -14,47 +13,27 @@ type jsonData struct {
 
 func socketandparse() {
 	socketPath := "/tmp/bulwark.sock"
-	if err := os.RemoveAll(socketPath); err != nil{
-		fmt.Println("Error delete old socket", err)
-		os.Exit(1)
-	}
 
-	listener, err := net.Listen("unix", socketPath)
-	if err != nil {
-		fmt.Println("error for make socket", err)
-		os.Exit(1)
+	fmt.Printf("Connect to socket: %s", socketPath)
+
+	conn, err := net.Dial("unix", socketPath); 
+	if err != nil{
+		fmt.Printf("\tFailed to connect to the socket: %v\n", err)
+		return
 	}
-	defer listener.Close()
+	defer conn.Close()
 
 	fmt.Printf("[Сервер] Слушаю сокет: %s\n", socketPath)
-
-	for {
-		// Получаем переменную conn от сокета
-		conn, err := listener.Accept()
-		if err != nil {
-			fmt.Println("error for connect: ", err)
-			continue
-		}
-
-		// Передаем переменную conn дальше в обработчик
-		go handleClient(conn) 
-	}
-}
-
-func handleClient(conn net.Conn) {
-	defer conn.Close()
-	fmt.Println("[Сервер] Клиент подключился, ожидаю поток JSON...")
-
 	decoder := json.NewDecoder(conn)
-
 	for {
-		var data UserData
+		var data jsonData
+
 		err := decoder.Decode(&data)
-		if err != nil {
-			fmt.Println("[Сервер] Соединение закрыто или ошибка:", err)
+		if err != nil{
+			fmt.Printf("error", err)
 			break
 		}
 
-		fmt.Printf("[Сервер] Получены новые данные: %+v\n", data)
+		fmt.Printf("")
 	}
 }
