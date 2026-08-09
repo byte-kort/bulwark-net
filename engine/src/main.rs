d198899572a6734c267cd0ba36ce13b03d31fb89
Panfes <pan.fedor197@gmail.com>
2026-08-09 23:17:30 +0300

    refactor: socket binding replaced to start of main

--- engine/src/main.rs
+++ engine/src/main.rs
@@ -9,6 +9,11 @@ use crate::network::models::NetworkState;
 use crate::network::interface::NetFace;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let path = "/tmp/bulwark.sock";
+
+    let _ = std::fs::remove_file(path);
+    let listener = UnixListener::bind(path)?;
+
     let mut state = NetworkState::new();
     let iface = NetFace::get_my_iface();
 
@@ -21,10 +26,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         state.add_device(device);
     }
 
-    let path = "/tmp/bulwark.sock";
-
-    let _ = std::fs::remove_file(path);
-    let listener = UnixListener::bind(path)?;
 
     println!("[#] Waiting for connection");
     let (mut stream, _) = listener.accept()?;
