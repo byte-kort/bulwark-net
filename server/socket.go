815e50ada833ecf86d0fc0e982147d70b2c2429c
ramik12345 <rramikpv@gmail.com>
2026-08-10 00:23:08 +0300

    разделил парсер и связь с сокетом на 2 файла

--- server/socket.go
+++ server/socket.go
@@ -6,38 +6,6 @@ import (
 	"net"
 	"time"
 )
-
-type jsonData struct {
-	IP net.IP `json:"ip"`
-	Mac net.HardwareAddr `json:"mac"`
-}
-
-
-func (j *jsonData) UnmarshalJSON(data []byte) error {
-	type Alias jsonData
-	aux := struct{
-		IP string `json:"ip"`
-		Mac string `json:"mac"`
-		*Alias
-	}{
-		Alias: (*Alias)(j),
-	}
-	if err := json.Unmarshal(data, &aux); err != nil{
-		return err
-	}
-	parsedIP := net.ParseIP(aux.IP)
-	if parsedIP == nil || parsedIP.To4() == nil{
-		return  fmt.Errorf("invalid or non-ipv4 address: %s", aux.IP)
-	}
-	j.IP = parsedIP.To4()
-	parsedMAC, err := net.ParseMAC(aux.Mac)
-	if err != nil{
-		return fmt.Errorf("invalid MAC address %s: %w", aux.Mac, err)
-	}
-	j.Mac = parsedMAC
-	return nil
-}
-
 func socketandparse() {
 	socketPath := "/tmp/bulwark.sock"
 	conn, err := net.Dial("unix", socketPath); 
